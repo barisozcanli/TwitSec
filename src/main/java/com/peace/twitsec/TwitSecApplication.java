3420eab464166cbdbdf6178ecba52c1bfbe56dbc
Barış Özcanlı <barisozcanli@gmail.com>
2016-03-08 23:33:28 +0200

    Test Controller added

--- src/main/java/com/peace/twitsec/TwitSecApplication.java
+++ src/main/java/com/peace/twitsec/TwitSecApplication.java
@@ -1,11 +1,13 @@
 package com.peace.twitsec;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.peace.*")
+@EnableAutoConfiguration
 public class TwitSecApplication {
 
     public static void main(String args[]) {
